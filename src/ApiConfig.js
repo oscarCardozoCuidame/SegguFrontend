c11904668713dcb1489cf21922b7882cccd4131a
oscarCardozoCuidame <ocardozo@cuidame.tech>
2024-12-26 19:34:50 -0500

    Update ApiConfig.js

--- src/ApiConfig.js
+++ src/ApiConfig.js
@@ -1,9 +1,9 @@
 let API_BASE_URL;
 
 if (process.env.NODE_ENV === 'production') {
-  API_BASE_URL = 'https://mi-api-produccion.com/api';
+  API_BASE_URL = 'https://seggubackend-production.up.railway.app/seggu';
 } else {
-  API_BASE_URL = 'http://localhost:4000/seggu';
+  API_BASE_URL = 'https://seggubackend-production.up.railway.app/seggu';
 }
 
 export default API_BASE_URL;
