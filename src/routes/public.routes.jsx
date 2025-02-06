c39977ccc971fb60aefdfe6b1f4c6f1f39b3f0fe
oscarCardozoCuidame <ocardozo@cuidame.tech>
2025-02-06 15:59:27 -0500

    Change Principal Path
    Se configuro para que la redireccion principal sea ./inicio

--- src/routes/public.routes.jsx
+++ src/routes/public.routes.jsx
@@ -1,17 +1,18 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import PaginaInicialLayout from "../pages/PaginaInicial/PaginaInicial.layout";
 import PaginaHome from "../pages/home/home";
-import PaginaPerfil from "../pages/perfil/PaginaPerfil";
 
 const PublicRoutes = () => {
   return (
     <Routes>
+      <Route path="/" element={<Navigate to="/inicio" replace />} />
+      
       <Route path="/inicio/*" element={<PaginaInicialLayout />} />
       <Route path="/home/*" element={<PaginaHome />} />
-      {/* <Route path="/profile/*" element={<PaginaPerfil />} /> */}
     </Routes>
   );
 };
 
 export default PublicRoutes;
+
